e686dc3035b8073bfcaa1c93d4a4937d740ae48d
AbisheK0726 <abishekaneese726@gmail.com>
2022-10-12 17:45:18 +0100

    Added comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,10 @@
-#Create FizzBuzz program
-
 def main():
     for i in range(1,101):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0: # if i is divisible by 3 and 5
             print("FizzBuzz")
-        elif i % 3 == 0:
+        elif i % 3 == 0: # if i is divisible by 3
             print("Fizz")
-        elif i % 5 == 0:
+        elif i % 5 == 0: # if i is divisible by 5
             print("Buzz")
         else:
             print(i)
